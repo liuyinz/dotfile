6026ec3a8e9f3417a0b3f419ce0a3a911d9e3d26
liuyinz <liuyinz95@gmail.com>
2024-01-28 23:42:15 +0800

    update emacs version to 30 in install script

--- tool/emacs.sh
+++ tool/emacs.sh
@@ -4,17 +4,17 @@ cd "$(dirname "${BASH_SOURCE[0]}")" && source "./utils.sh"
 
 install_emacs() {
 
-  if ! brew list --version emacs-head@28 >/dev/null 2>&1; then
-    print_in_yellow "   emacs-head@28: install start ...\n\n"
-    brew install emacs-head@28 \
+  if ! brew list --version emacs-head@30 >/dev/null 2>&1; then
+    print_in_yellow "   emacs-head@30: install start ...\n\n"
+    brew install emacs-head@30 \
       --with-cocoa \
-      --with-native-comp \
-      --with-native-full-aot \
-      --with-pdumper \
-      --with-xwidgets
+      --with-xwidgets \
+      --with-mailutils \
+      --with-tree-sitter \
+      --with-poll
   fi
 
-  print_result $? "emacs-head@28: install done"
+  print_result $? "emacs-head@30: install done"
 
 }
 
@@ -23,30 +23,30 @@ clone_emacs() {
 
   if [[ ! -d "$emacs_dir/.git" ]]; then
     [ -d "$emacs_dir" ] && mv "$emacs_dir" "$HOME/$dotcache/backup/"
-    print_in_yellow "   emacs-head@28: clone start ...\n\n"
+    print_in_yellow "   emacs-head@30: clone start ...\n\n"
     command git clone https://github.com/liuyinz/emacs.d.git "$emacs_dir"
   fi
 
-  print_result $? "emacs-head@28: clone done"
+  print_result $? "emacs-head@30: clone done"
 
 }
 
 submodule_emacs() {
   local emacs_dir="$HOME/.config/emacs"
 
-  print_in_yellow "   emacs-head@28: submodule start ...\n\n"
+  print_in_yellow "   emacs-head@30: submodule start ...\n\n"
   git -C "$emacs_dir" submodule update --init --jobs "$(nproc)"
-  print_result $? "emacs-head@28: submodule done"
+  print_result $? "emacs-head@30: submodule done"
 }
 
 main() {
-  print_in_purple "\n   emacs-head@28: install check ...\n\n"
+  print_in_purple "\n   emacs-head@30: install check ...\n\n"
   install_emacs
 
-  print_in_purple "\n   emacs-head@28: clone check ...\n\n"
+  print_in_purple "\n   emacs-head@30: clone check ...\n\n"
   clone_emacs
 
-  print_in_purple "\n   emacs-head@28: submodule check ...\n\n"
+  print_in_purple "\n   emacs-head@30: submodule check ...\n\n"
   submodule_emacs
 }
 
