9f998d3e08d745bec65d272228aa9e30cf91a6da
liuyinz <liuyinz95@outlook.com>
2024-05-28 11:32:35 +0800

    update zsh/autosuggestion configs

--- source/home_air15/zsh/init.zsh
+++ source/home_air15/zsh/init.zsh
@@ -174,7 +174,8 @@ command -v mise >/dev/null || curl https://mise.run | sh
 # autosuggestions
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 ZSH_AUTOSUGGEST_USE_ASYNC=1
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#808080,underline"
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 # autoupdate
 ZSH_CUSTOM_AUTOUPDATE_QUIET=true
