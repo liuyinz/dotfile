2a33564c0da7131d606c1e3014562c2a6870ac90
liuyinz <liuyinz95@outlook.com>
2024-05-28 11:34:49 +0800

    update prettier global config

--- source/base/.prettierrc.js
+++ source/base/.prettierrc.js
@@ -2,7 +2,7 @@
 // .prettierrc.js
 module.exports = {
   // 一行最多 120 字符
-  printWidth: 120,
+  printWidth: 90,
   // 使用 2 个空格缩进
   tabWidth: 2,
   // 不使用缩进符，而使用空格
@@ -14,7 +14,7 @@ module.exports = {
   // 对象的 key 仅在必要时用引号
   quoteProps: 'as-needed',
   // jsx 不使用单引号，而使用双引号
-  jsxSingleQuote: false,
+  jsxSingleQuote: true,
   // 末尾需要有逗号
   trailingComma: 'all',
   // 大括号内的首尾需要空格
@@ -22,7 +22,7 @@ module.exports = {
   // jsx 标签的反尖括号不需要换行
   bracketSameLine: true,
   // 箭头函数，只有一个参数的时候，也需要括号
-  arrowParens: 'always',
+  arrowParens: 'avoid',
   // 每个文件格式化的范围是文件的全部内容
   rangeStart: 0,
   rangeEnd: Infinity,
